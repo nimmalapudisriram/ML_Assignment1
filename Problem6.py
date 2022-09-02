7eb1c5b3eebbab2c7eb060839f19050370ca1cef
nimmalapudisriram <112573855+nimmalapudisriram@users.noreply.github.com>
2022-09-01 21:44:13 -0500

    Delete Problem6.py

--- Problem6.py
+++
@@ -1,8 +0,0 @@
-"""How many unique words have been used in the sentence? Use the split methods and set
-to get the unique words """
-
-sentence = "I am a teacher and I love to inspire and teach people"
-txt = sentence.split(" ")
-unique_words = set(txt)
-print("\nNo of unique words in the sentence: ", len(unique_words))
-print("\nSet with unique words: ", unique_words)
